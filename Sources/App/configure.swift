2ecda59db54f179293f97d910667f2fc8b3db10c
hien.tran <hien.tran@be.xyz>
2021-02-17 00:38:56 +0700

    configured heroku database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,13 +4,35 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
+    if let url = Environment.get("DATABASE_URL") {
+        app.databases.use( try .postgres(url: url), as: .psql)
+    } else {
+        let databaseName: String
+        let databasePort: Int
+
+        if app.environment == .testing {
+            databaseName = "vapor_test"
+            if let testPort = Environment.get("DATABASE_PORT") {
+                databasePort = Int(testPort) ?? 5433
+            } else {
+                databasePort = 5433
+            }
+        } else {
+            databaseName = "vapor_database"
+            databasePort = 5432
+        }
+
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: databasePort,
+            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+            database: databaseName
+        ), as: .psql)
+    }
+
+
+
 
     app.migrations.add(CreateUser())
     app.migrations.add(CreateQuote())
